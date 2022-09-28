f0e2bd673c83b1f54f0ed9e462a38ae37b63111c
albertmary <81420434+albertmary@users.noreply.github.com>
2022-09-28 18:26:14 +0100

    Update 1-main.c

--- 0x08-recursion/1-main.c
+++ 0x08-recursion/1-main.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+
+    _print_rev_recursion("\nColton Walker");
+
+    return (0);
+
+}
